eee658e21e14cfe06ad821d3eb5ecfdb90c4e4d3
Michał Radkiewicz <michal.radkiewicz.araz@gmail.com>
2023-01-23 11:08:49 +0100

    Init projest 3

--- zadanie.py
+++ zadanie.py
@@ -1,3 +1,7 @@
 zakupy_lista = {"piekarnia": ["chleb", "bułki", "pączek"],
                 "warzywniak": ["marchew", "seler", "rukola"]
 }
+for i in zakupy_lista:
+    rzeczy = zakupy_lista[i]
+    druk = f'Idę do sklepu {i.capitalize()} i kupuję tu następujące rzeczy:{rzeczy}'
+    print(druk)
