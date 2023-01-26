3e0b00b7559addbd947b12231bea05ded90a3a0b
michalradkiewicz <123367243+michalradkiewicz@users.noreply.github.com>
2023-01-26 18:02:16 +0100

    Add list

--- zadanie.py
+++ zadanie.py
@@ -5,3 +5,5 @@ for i in zakupy_lista:
     rzeczy = zakupy_lista[i]
     druk = f'Idę do sklepu {i.capitalize()} i kupuję tu następujące rzeczy:{rzeczy}'
     print(druk)
+   
+  list = [1, 2, "nożyce", "szczypce", "grabki"]
